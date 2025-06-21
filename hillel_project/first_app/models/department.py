1b2bd106188804c84d7e2558ccb2495b6477bbd2
Nikolay347 <nickpkinn@gmail.com>
2025-06-21 16:57:16 +0300

    added verbose_name in models for translation

--- hillel_project/first_app/models/department.py
+++ hillel_project/first_app/models/department.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 class Department(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(verbose_name=_("name"),max_length=200)
     parent_department = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
--- hillel_project/first_app/models/employee.py
+++ hillel_project/first_app/models/employee.py
@@ -4,4 +4,4 @@ from django.db import models
 class Employee(AbstractUser):
     birth_date =  models.DateField(null=True, blank=True)
     hire_date =  models.DateField(null=True, blank=True)
-    position = models.ForeignKey('Position', on_delete=models.SET_NULL, null=True)
+    position = models.ForeignKey("Position", on_delete=models.SET_NULL, null=True)
